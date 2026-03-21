876ed36550129af9603689396d441f4b2b384c39
Diego Lemus <diego.lemus@iteso.mx>
2026-03-21 11:34:17 -0600

    secured my env variables and added a gitignore

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import requests
 import json
+from datetime import datetime, timezone
+import pandas as pd
+import os
 
-CANVAS_URL = "https://canvas.iteso.mx/"
-API_TOKEN = "12734~BYDFyTM2xLAyRzQUFuzu7ePamVNfrMQeKCxR6RFMQeHUQtzHee3vZ7vxF62uZcBH"
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+CANVAS_URL = os.getenv("CANVAS_URL")
+CANVAS_TOKEN = os.getenv("CANVAS_TOKEN")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 response = requests.get(
     f"{CANVAS_URL}api/v1/courses",
     headers={"Authorization": f"Bearer {API_TOKEN}"},
-    params={"enrollment_type":"student", "per_page": 10},
+    params={"enrollment_type":"student", "per_page": 100, "enrollment_term_id": 326},
 )
 
 #print(json.dumps(response.json(), indent=2))
@@ -15,6 +24,32 @@ response = requests.get(
 for course in response.json():
     if "name" not in course:
         continue
-    print(course["id"],course["name"])
+    if "P2026" not in course["name"]:
+        continue
+    print(course["id"], course["name"])
 
+
+# ASSIGNMENTS
+print("="*30)
+course_id = "55069"
+response2 = requests.get(
+    f"{CANVAS_URL}api/v1/courses/{course_id}/assignments",
+    headers={"Authorization": f"Bearer {API_TOKEN}"},
+    params={"order_by":"due_at", "per_page": 100},
+)
+
+#print(json.dumps(response2.json(), indent=2))
+
+now = datetime.now(timezone.utc)
+
+
+for assignment in response2.json():
+    if assignment["due_at"] != None:
+
+        due_at_str = assignment["due_at"] 
+        due_at = datetime.fromisoformat(due_at_str.replace("Z", "+00:00"))
+
+        if assignment["has_submitted_submissions"]==False and due_at > now:
+            print(assignment["name"])
+        
 
